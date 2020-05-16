3dc7205e4763641894c60a6d9ac8b275566591e6
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2020-05-16 20:19:46 +0200

    still looking for bad reasoning in wandering robot

--- 2020-round-b/wandering-robot-14pts-24pts/wandering-robot.py
+++ 2020-round-b/wandering-robot-14pts-24pts/wandering-robot.py
@@ -6,7 +6,10 @@ MAX_W = 100000
 
 
 def L2F():
-    result = [0, 0]  # lg(0!), lg(1!)
+    """
+    Precompute array lg(0!), lg(1!),..., lg((MAX_H+MAX_W-2)!)
+    """
+    result = [0, 0]
     i = 2
     while i <= MAX_H + MAX_W - 2:
         result.append(result[-1] + log2(i))
@@ -19,7 +22,7 @@ def entering_field_probability(w, h):
 
 
 def success_probability(W, H, L, U, R, D):
-    success = 0
+    success = 0.0
     while L > 1 and D < H:
         (L, D) = (L - 1, D + 1)
         success += entering_field_probability(L, D)
@@ -27,6 +30,21 @@ def success_probability(W, H, L, U, R, D):
         (R, U) = (R + 1, U - 1)
         success += entering_field_probability(R, U)
     return success
+
+
+def success_probability_2(W, H, L, U, R, D):
+    failure = 0.0
+    print("Right top")
+    for i in range(R - L + 1):  # go right top edge
+        entp = entering_field_probability(L + i, U)
+        print("P({}, {})={}".format(L + i, U, entp))
+        failure += entp
+    print("Left to down")
+    for i in range(D - U):  # go down left edge
+        entp = entering_field_probability(L, U+i+1)
+        print("P({}, {})={}".format(L, U+i+1, entp))
+        failure += entp
+    return 1-failure
 
 
 L2F = L2F()
